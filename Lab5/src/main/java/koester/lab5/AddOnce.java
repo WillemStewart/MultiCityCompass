24e18adec92c34d2c7b8ff2a510db04d22285d85
Willem Stewart <100379801+WillemStewart@users.noreply.github.com>
2023-12-05 13:45:40 -0600

    Delete Lab5 directory

--- Lab5/src/main/java/koester/lab5/AddOnce.java
+++
@@ -1,6 +0,0 @@
-//Interface AddOnce.java
-package koester.lab5;
-
-interface AddOnce <E extends Comparable<? super E>> { 
-     public E AddOnce(E item);
-}
--- Lab5/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module koester.lab5 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens koester.lab5 to javafx.fxml;
-    exports koester.lab5;
-}
